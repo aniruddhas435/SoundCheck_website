ff5c562ef9a68248fe843b046500760ab86ec68d
Aniruddha Sarkar <aniruddhas435@gmail.com>
2021-06-13 05:45:21 +0530

    trying to fix the ref.current = null issue in SoundCheckApp.js

--- src/components/ResultSyntaxConsole.jsx
+++ src/components/ResultSyntaxConsole.jsx
@@ -1,19 +1,25 @@
-import { React, useRef, useEffect } from 'react';
+import { React, useRef} from 'react';
 import LoadWithShadow from './LoadWithShadow';
 
 const ResultSyntaxConsole = ({isLoading, result, captureWidth}) => {
     const ssTextRef = useRef();
 
-    useEffect(() => {
-        captureWidth(ssTextRef.current.clientWidth / ssTextRef.current.textContent.length);
-    });
+    const handleTextLoaded = (event) => {
+        captureWidth(event.target.clientWidth / event.target.value.length);
+    };
 
     return (
         <div className="result styled-scrollbar" key="result-syntax">
             <LoadWithShadow
             isLoading={isLoading}
             />
-            <div ref={ssTextRef} style={{width: 'fit-content'}}>{result['scaledSyntax']}</div>
+            <div 
+            ref={ssTextRef} 
+            style={{width: 'fit-content'}}
+            onLoad={event => handleTextLoaded(event)}
+            >
+                {result['scaledSyntax']}
+            </div>
         </div>
     );
 };
