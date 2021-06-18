8201383de105f8d52c516e95072d9511f001f861
Aniruddha Sarkar <aniruddhas435@gmail.com>
2021-06-18 15:43:06 +0530

    changing scaledSyntax structure

--- src/components/RenderExecutedCFG.jsx
+++ src/components/RenderExecutedCFG.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RenderExecutedCFG = ({scaledSyntax}) => {
     return (
         <div>
-            {scaledSyntax}
+            {scaledSyntax.stringify()}
         </div>
     );
 }
