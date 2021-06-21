041f7409057c3a0b4c43a53bb1c4140e2935d196
Aniruddha Sarkar <aniruddhas435@gmail.com>
2021-06-21 19:42:05 +0530

    new files were not added last time by mistake

---
+++ src/components/RenderSyntaxComponent.jsx
@@ -0,0 +1,81 @@
+import { React, useState } from 'react';
+
+const RenderSyntaxComponent = ({content, header, leftSpaces}) => {
+    const type = typeof(content);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const blockStyle = {
+        display: 'flex',
+        paddingLeft: '' + (leftSpaces * 3) + 'em'
+    };
+
+    const textStyle = {
+        paddingTop: '6.5px'
+    };
+
+    if(type === 'string' || type === 'number') {
+        const leafStyle = {
+            padding: 0
+        };
+
+        return (
+            <div style={blockStyle}>
+                <div style={leafStyle}>{header}: {content}</div>
+            </div>
+        );
+    } else if(header === 'distribution') {
+        const contentStyled = Object.entries(content).map(([key, value]) => {
+            return (
+                <RenderSyntaxComponent
+                    content={value}
+                    header={key}
+                    leftSpaces={leftSpaces}
+                />
+            );
+        });
+
+        return (
+            <div>
+                {contentStyled}
+            </div>
+        );
+    } else if(header === 'values' || header === 'empty') {
+        return (<div></div>);
+    } else {
+        const handleExpand = () => setIsOpen(!isOpen);
+        const contentStyled = Object.entries(content).map(([key, value]) => {
+            return (
+                <RenderSyntaxComponent
+                    content={value}
+                    header={key}
+                    leftSpaces={leftSpaces + 1}
+                />
+            );
+        });
+
+        return (
+            <div>
+                <div style={blockStyle}>
+                    <button onClick={handleExpand} className="left-arrow">
+                        <span className="material-icons">
+                            {isOpen ? 'arrow_drop_down' : 'arrow_right'}
+                        </span>
+                    </button>
+                    <div style={textStyle}>{header}</div>
+                </div>
+
+                {/* {isOpen ? content.forEach(([key, value]) => (
+                    <RenderSyntaxComponent
+                        content={value}
+                        header={key}
+                        leftSpaces={leftSpaces + 1}
+                    />
+                )) : (<div></div>)} */}
+
+                {isOpen ? contentStyled : (<div></div>)}
+            </div>
+        );
+    }
+};
+ 
+export default RenderSyntaxComponent;
