c368b83b45a339bbcfdb9836b0803dd929761b78
Aniruddha Sarkar <aniruddhas435@gmail.com>
2021-06-28 04:41:41 +0530

    testing the postSyntax url

--- src/SoundCheckApp.js
+++ src/SoundCheckApp.js
@@ -154,8 +154,7 @@ async function postSyntax(url, body) {
     const response = await fetch(url, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json, text/plain, */*',
-            
+            'Content-Type': 'application/json'
         },
         // redirect: 'follow',
         // referrerPolicy: 'no-referrer',
